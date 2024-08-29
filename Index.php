6d57433d9fe71f8c7cd07ca2b46d1c6a0b221d71
heroku-657 <trx67t@gmail.com>
2024-08-30 05:55:08 +0600

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,61 @@
+<html> <head> <html><head><meta name="viewport" content="width=device-width"> <title>Enjoy</title></title> <meta name="viewport" content="width=device-width"> <link href="https://fonts.googleapis.com/css?family=Laila|Allura|Rancho|Mr+Dafoe|Nothing+You+Could+Do|Oleo+Script|Pinyon+Script|Fondamento|Lobster|Yatra+One|Jacques+Francois+Shadow|Faster+One|Ubuntu|Black+Ops+One|Saira+Stencil+One|Monoton|Ewert|Press+Start+2P" rel="stylesheet"><center><div class='error'><Div></div><Div></div>  <style> body { background:Orange ; font-family:Laila; font-weight:normal; font-size:20; color:Black; } div { Background:Orange; margin:10px; <h> Telegram Free Post Views By @coderajinkya</h>
+Border: 3px Solid #EAEFFA; Border-radius:20px 20px 20px 20px; Font-Family:Laila; Font-weight:bold; Font-size:20; } .error { Background:#EBF1FC; margin:2px; Border: 4px Solid Black; Border-radius:10px 10px 10px 10px; font-Family:Laila; Font-weight:bold; Font-size:19; } input[type=submit],select { width:60%; Margin:5%; height:60px; background:white; Color:Black; font-weight:Bold; font-size:18; font-family:Laila; border:5px solid black ; border-radius: 20px 20px 20px 20px; } input[type=number], select { width: 80%; background-color:white ; font-family: Laila; color:#8E8E8E; padding: 15px 40px ; margin: 20px 10px; display: inline-block; border: 5px solid Black ; border-radius: 10px 10px 10px 10px ; box-sizing: border-box; } input[type=password], select { width: 80%; background-color:white ; font-family: Laila; color:red; padding: 15px 40px ; margin: 20px 10px; display: inline-block; border: 5px solid #FFFFFF ; border-radius: 10px 10px 10px 10px ; font-weight:Bold; box-sizing: border-box; } input[type=text], select { width: 80%; background-color:white ; font-family: Laila; color:#898989; Font-weight:bold; padding: 15px 40px ; margin: 20px 10px; display: inline-block; border: 5px solid Black ; border-radius: 10px 10px 10px 10px ; box-shadow: ; box-sizing: border-box; } hr{border: 0; height: 2; color: ; background-image:linear-gradient(to right, rgba(0,0,0,0) ,rgba(0,0,0,0.75), rgba(0,0,0,0)); } </style> </head><Hr>
+
+
+
+<body>
+
+<?php
+// i will provide you this file on telegram 
+
+echo "Free Telegram Post Views By @coderajinkya<br><br>";
+//You Can Replace This With Your Script If Your Script Send Instagram Likes Then Write Instgram Like Script And Add Your Credit Now Your Work Done Just upload it on any web host ( profreehost , infinity free , 000webhhost etc..) i will use local host for Show You lets cheak It
+
+if(isset($_GET['submit'])){
+    $link = $_GET['link'];
+    $url = "https://imperialsmm.in/api/v2";
+    $apiKey = "1e7c93e15dcda1c692822874e5f1e9bd";
+  
+    // enter service id here ðŸ‘‡
+        $serviceId = 165;
+        
+        // enter max amount here ðŸ‘‡
+    $maxAmount = 100;
+    
+    //now your work done lets add your telegram chennal link
+
+    // Prepare data for the API request
+    $data = 'key=' . $apiKey . '&action=add&service=' . $serviceId . '&link=' . $link . '&quantity=' . $maxAmount;
+
+    // Initialize cURL session
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    // Execute the cURL request and handle the response
+    $output = curl_exec($ch);
+    $json = json_decode($output, true);
+    curl_close($ch);
+
+    // Display results
+    echo "<br> $output   <br> Done âœ… : $maxAmount <br> Target ðŸŽ¯ : $link <br> ";
+     
+} else {
+    // Display the form if 'submit' is not set
+    echo "<form action='' method='get'>
+          <input type='text' name='link' class='text' placeholder='telegram post link' required><br><br>
+          <input type='submit' class='submit' name='submit' value='submit'>";
+}
+
+?>
+
+
+
+</body>
+<br>
+<center><script type='text/javascript'>(function() {var script=document.createElement('script');script.type='text/javascript';script.async =true;script.src='//telegram.im/widget-button/index.php?id=@taIkgroup';document.getElementsByTagName('head')[0].appendChild(script);})();</script><a href='https://telegram.dog/taikgroup' target='_blank' class='telegramim_button telegramim_shadow telegramim_pulse' style='font-size:15px;max-width:300px;height:35px;background:red;box-shadow:3px 3px 3px red;color:white;border-radius:10px;' title='Join Now'><i>Telegram </i> <small><span class='telegramim_count' data-for='@taikgroup'></span></small></a></center>
+
+</html> 
